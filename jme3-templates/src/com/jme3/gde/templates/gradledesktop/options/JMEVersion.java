467d13b647fe8ab0fc5714a93a124d85df50356e
Toni Helenius <helenius.toni@gmail.com>
2026-01-07 17:22:10 +0200

    Add 3.8.1 to known jME versions

--- jme3-templates/src/com/jme3/gde/templates/gradledesktop/options/JMEVersion.java
+++ jme3-templates/src/com/jme3/gde/templates/gradledesktop/options/JMEVersion.java
@@ -56,6 +56,8 @@ package com.jme3.gde.templates.gradledesktop.options;
  */
 public enum JMEVersion implements LibraryVersion {
 
+    JME_3_8_1("3.8.1-stable",
+            "/com/jme3/gde/templates/files/patchnotes/381-stable.html"),
     JME_3_8_0("3.8.0-stable",
             "/com/jme3/gde/templates/files/patchnotes/380-stable.html"),
     JME_3_7_0("3.7.0-stable",
