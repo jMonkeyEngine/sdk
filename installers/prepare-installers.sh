2b32f1f692a146e6355a1406aa4413f00a258d30
Toni Helenius <helenius.toni@gmail.com>
2026-06-01 22:02:25 +0300

    Tweaks

--- installers/prepare-installers.sh
+++ installers/prepare-installers.sh
@@ -32,7 +32,7 @@ function prepare_nbpackage {
         echo "< Already existing, SKIPPING."
     else
         unzip -qq downloads/nbpackage.zip -d downloads/nbpackage
-        mv downloads/nbpackage/nbpackage-$nbpackage_version/. nbpackage/
+        mv downloads/nbpackage/nbpackage-$nbpackage_version/* nbpackage/
         echo "< OK!"
     fi
 }
