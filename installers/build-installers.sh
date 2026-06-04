b5819c3e6c68f0acb94bdf31dc4abf7832976fd3
Toni Helenius <helenius.toni@gmail.com>
2026-06-04 20:24:04 +0300

    Test path getting

--- installers/build-installers.sh
+++ installers/build-installers.sh
@@ -23,6 +23,9 @@ function build_nbpackage {
 function build_nbpackage_on_cmd {
     echo ">> Building the NBPackage installer in CMD for $1-$2"
 
+    isccPath=$(cygpath -w "$(command -v ISCC.exe)")
+    echo ">> ISCC found at $isccPath"
+
     mkdir -p ../dist/installers
     cmd.exe //c call .\\nbpackage\\bin\\nbpackage.cmd --input ../dist/jmonkeyplatform.zip --config "$1-$2/$3" --output ../dist/installers/ -v -Ppackage.version="$4"
 
